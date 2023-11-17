6251b8cf171c0d9af2feee47e833fd9d6b8d11b7
Eric Lam <elam4@uncc.edu>
2023-11-16 23:10:49 -0500

    -exercise Multi-point circle

---
+++ MultiPoint/MultiPoint.js
@@ -0,0 +1,85 @@
+// MultiPoint.js (c) 2012 matsuda
+// Vertex shader program
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'void main() {\n' +
+  '  gl_Position = a_Position;\n' +
+  '  gl_PointSize = 10.0;\n' +
+  '}\n';
+
+// Fragment shader program
+var FSHADER_SOURCE =
+  'void main() {\n' +
+  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+  '}\n';
+
+function main() {
+  // Retrieve <canvas> element
+  var canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // Write the positions of vertices to a vertex shader
+  var n = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the positions of the vertices');
+    return;
+  }
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0, 0, 0, 1);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Draw three points
+  gl.drawArrays(gl.POINTS, 0, n);
+}
+
+function initVertexBuffers(gl) {
+  var vertices = new Float32Array(100); // 50 vertices with x and y components
+  var n = 50; // The number of vertices
+
+  for (var i = 0; i < n; i++) {
+    var angle = (i / n) * 2 * Math.PI; // Angle for each vertex
+    var x = 0.5 * Math.cos(angle); // X component using the parametric equation
+    var y = 0.5 * Math.sin(angle); // Y component using the parametric equation
+
+    // Assign the coordinates to the vertices array
+    vertices[i * 2] = x;
+    vertices[i * 2 + 1] = y;
+  }
+
+  // Now you have an array of 100 elements (50 vertices with x and y components)
+  // Use this array for your 2D WebGL magic!
+}
+
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // Write date into the buffer object
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
+
+  return n;
+}
